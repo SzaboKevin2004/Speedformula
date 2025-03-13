dadd6afb3e953d14b6e19e9e4d122e8cc184229a
Szabó Kevin <szabo.kevin.2019@osztalyterem.katolikuskeri.hu>
2025-03-13 09:00:19 +0100

    Kliens kódok kommentelése

--- kliens/src/app/footer/footer.component.ts
+++ kliens/src/app/footer/footer.component.ts
@@ -1,3 +1,4 @@
+// Footer rész viselkedéséért felelős ts
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
